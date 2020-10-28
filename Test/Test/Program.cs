0077138d75a8245566c43376722b2b41b40a05f1
NikitaNask <n.naskalnyuk@mail.ru>
2020-10-28 15:03:36 +0300

    first

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Globalization;
 using System.Net.Sockets;
 using System.Text.RegularExpressions;
-
+using System.Configuration;
 
 namespace Test
 {
@@ -14,14 +14,24 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            TcpClient tcp = new TcpClient();
-            tcp.Connect("192.168.55.81", 6000);
-            NetworkStream nt = tcp.GetStream();
-            string str = "<request><groupOperId>319979257</groupOperId><KKMTYPE>ATOL</KKMTYPE><summ>0.16</summ><textCheck><email>111@aisgorod.ru</email><![CDATA[]]></textCheck><operatorName>ЛК</operatorName></request>";
-            byte[] bStr = Encoding.GetEncoding(1251).GetBytes(str);
-            nt.Write(bStr, 0, str.Length);
+            string ipAdress = ConfigurationManager.AppSettings["ipAdress"];
+            string ipPort = ConfigurationManager.AppSettings["ipPort"];
 
-
+            try
+            {
+                TcpClient tcp = new TcpClient();
+                tcp.Connect(ipAdress, Convert.ToInt32(ipPort));
+                NetworkStream nt = tcp.GetStream();
+                string str = "<request><groupOperId>1122336</groupOperId><KKMTYPE>ATOL</KKMTYPE><summ>0.16</summ><textCheck><email>111@aisgorod.ru</email><![CDATA[]]></textCheck><operatorName>ЛК</operatorName><accountNumber>aqwe12</accountNumber></request>";
+                byte[] bStr = Encoding.GetEncoding(1251).GetBytes(str);
+                nt.Write(bStr, 0, str.Length);
+                Console.ReadKey();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+                Console.Read();
+            }
         }
     }
 }
